0eb66a2fb3d3d1d4927e3883c317e2232afacae5
Erik <erik.monroy.an@gmail.com>
2023-12-06 10:58:38 -0600

    Dia 6 terminado

--- Dia6.js
+++ Dia6.js
@@ -1,47 +1,3 @@
-// function maxDistance(movements) {
-//     let steps = 0;
-//     let movementsArr = movements.split('');
-//     let isFrontLast = false;
-
-//     for (let index = 0; index < movementsArr.length; index++) {
-//         let movement = movementsArr[index];
-//         if (movement == '>') {
-//             steps++;
-//             isFrontLast = true;
-//         }
-//         if (movement == '<') {
-//             steps--;
-//             isFrontLast = false;
-//         }
-//         if (movement == '*' && isFrontLast) {
-//             steps++;
-//             isFrontLast = true;
-//         }
-//         if (movement == '*' && !isFrontLast) {
-//             steps--;
-//             isFrontLast = false;
-//         }
-//     }
-
-
-//     return (steps < 0) ? steps * -1 : steps;
-// }
-
-
-function maxDistance(movements) {
-    let movementsArr = movements.split('');
-    let forward = movementsArr.filter((element) => element == '>').length;
-    let back = movementsArr.filter((element) => element == '<').length;
-    let wildcard = movementsArr.filter((element) => element == '*').length;
-    forward = (forward > back || forward == back) ?
-        forward + wildcard : forward;
-    back = (back > forward) ? back + wildcard : back;
-    let totalSteps = forward - back
-    return (totalSteps < 0) ? totalSteps * -1 : totalSteps;
-}
-
-
-
 function maxDistance(movements) {
     let forward = 0;
     let back = 0;
@@ -56,7 +12,6 @@ function maxDistance(movements) {
     back = (back > forward) ? back + wildcard : back;
     return ((forward - back) < 0) ? (forward - back) * -1 : (forward - back);
 }
-
 
 
 const movements = '>>*<'
