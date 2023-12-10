7c17801ab084dbd9e7560da4c05f25999b4781a4
Erik <erik.monroy.an@gmail.com>
2023-12-09 23:46:37 -0600

    Actualizacion del dia 9

--- Dia9.js
+++ Dia9.js
@@ -1,33 +1,25 @@
-// function adjustLights(lights) {
-//     let caminoRojo = 0;
-//     let caminoVerde = 0;
-//     lights.forEach(function(light, index) {
-//         let isPair = (index % 2) == 0;
-//         if (isPair && light != '🔴') caminoRojo++;
-//         if (!isPair && light != '🟢') caminoRojo++;
-//         if (isPair && light != '🟢') caminoVerde++;
-//         if (!isPair && light != '🔴') caminoVerde++;
-//     });
-//     return Math.min(caminoRojo, caminoVerde);
-// }
-
 function adjustLights(lights) {
     let caminoRojo = 0;
     let caminoVerde = 0;
     lights.forEach(function(light, index) {
         let isPair = (index % 2) == 0;
-        if (isPair && light != '🔴') {
-            caminoRojo++;
-        } else if (isPair && light != '🟢') {
-            caminoVerde++;
-        }
-        if (!isPair && light != '🟢') {
-            caminoRojo++;
-        } else if (!isPair && light != '🔴') {
-            caminoVerde++;
-        }
+        if (isPair && light != '🔴') caminoRojo++;
+        if (!isPair && light != '🟢') caminoRojo++;
+        if (isPair && light != '🟢') caminoVerde++;
+        if (!isPair && light != '🔴') caminoVerde++;
     });
     return Math.min(caminoRojo, caminoVerde);
 }
 
-console.log(adjustLights(['🟢', '🔴', '🟢', '🟢', '🟢']));
+
+adjustLights(['🟢', '🔴', '🟢', '🟢', '🟢'])
+    // -> 1 (cambias la cuarta luz a 🔴)
+
+adjustLights(['🔴', '🔴', '🟢', '🟢', '🔴'])
+    // -> 2 (cambias la segunda luz a 🟢 y la tercera a 🔴)
+
+adjustLights(['🟢', '🔴', '🟢', '🔴', '🟢'])
+    // -> 0 (ya están alternadas)
+
+adjustLights(['🔴', '🔴', '🔴'])
+    // -> 1 (cambias la segunda luz a 🟢)
