c60915281902856ed08acb0cb27887f31b13ac15
Erik <erik.monroy.an@gmail.com>
2023-12-12 12:34:48 -0600

    Finalizacion del dia 11

--- Dia11.js
+++ Dia11.js
@@ -1,154 +1,71 @@
-// function getIndexsForPalindrome(word) {
-//     let letters = word.split('');
-//     let reverseWord = word.split('').reverse();
-//     let totalLetters = [];
-//     let letrasImpares = 0;
-//     let tratacambios = 0;
-
-//     // Son iguales
-//     if (word == reverseWord.join('')) return [];
-
-//     for (let index = 0; index < letters.length; index++) {
-//         const element = letters[index];
-//         const indexList = totalLetters.findIndex(object => object.letter === element);
-//         if (indexList >= 0) {
-//             totalLetters[indexList].count++;
-//             if ((totalLetters[indexList].count % 2) == 0) {
-//                 letrasImpares--;
-//             } else {
-//                 letrasImpares++;
-//             }
-//         } else {
-//             totalLetters.push({ letter: element, count: 1 });
-//             letrasImpares++;
-//         }
-//     }
-//     // Verificacion si no es palindromo
-//     const esParTotalLetras = letters.length;
-//     if (((esParTotalLetras % 2) != 0 && letrasImpares == 1)) {
-//         // console.log('Puede ser ');
-
-//         for (let index = 0; index < letters.length; index++) {
-//             let indiceDerecho = index;
-//             let indiceIzquierdo = (letters.length - 1) - index;
-//             let indiceCambia = 0;
-
-//             let letraUno = letters[indiceDerecho];
-//             let letraDos = letters[indiceIzquierdo];
-//             if (letraUno != letraDos) {
-//                 // Si las letras son diferentes vamos a obtener
-//                 indiceCambia = letters.indexOf(letraDos);
-//                 if (indiceCambia == indiceIzquierdo) {
-//                     indiceCambia = reverseWord.indexOf(letraUno)
-//                     return [indiceCambia, indiceIzquierdo]
-//                 } else {
-//                     let comparacion1 = word.split('')
-//                     comparacion1[indiceDerecho] = letters[indiceCambia]
-//                     comparacion1[indiceCambia] = letters[indiceDerecho]
-//                     let reverseWordUltimo = [...comparacion1].reverse();
-
-//                     for (let index = 0; index < reverseWordUltimo.length; index++) {
-//                         if (comparacion1[index] != reverseWordUltimo[index]) {
-//                             return null
-//                         }
-//                     }
-//                     return [indiceDerecho, indiceCambia];
-//                 }
-//             }
-//         }
-//     }
-//     if ((esParTotalLetras % 2) == 0 && letrasImpares == 0) {
-//         // console.log('Puede ser ');
-//         // return;
-
-//         for (let index = 0; index < letters.length; index++) {
-//             let indiceDerecho = index;
-//             let indiceIzquierdo = (letters.length - 1) - index;
-//             let indiceCambia = 0;
-
-//             let letraUno = letters[indiceDerecho];
-//             let letraDos = letters[indiceIzquierdo];
-//             if (letraUno != letraDos) {
-//                 // Si las letras son diferentes vamos a obtener
-//                 indiceCambia = letters.indexOf(letraDos);
-//                 if (indiceCambia == indiceIzquierdo) {
-//                     indiceCambia = reverseWord.indexOf(letraUno)
-//                     return [indiceCambia, indiceIzquierdo]
-//                 } else {
-//                     return [indiceDerecho, indiceCambia];
-//                 }
-//             }
-//         }
-//     }
-
-//     return null;
-// }
-
-// console.log(getIndexsForPalindrome('aaababa'));
-// console.log(getIndexsForPalindrome('rotaratov'));
-
-
-
 function getIndexsForPalindrome(word) {
     let letters = word.split('');
     let reverseWord = word.split('').reverse();
-    let totalLetters = [];
-    let letrasImpares = 0;
+    let lettersCount = [];
+    let notPairsCount = 0;
     // Son iguales
     if (word == reverseWord.join('')) return [];
     for (let index = 0; index < letters.length; index++) {
         const element = letters[index];
-        const indexList = totalLetters.findIndex(object => object.letter === element);
+        const indexList = lettersCount.findIndex(object => object.letter === element);
         if (indexList >= 0) {
-            totalLetters[indexList].count++;
-            if ((totalLetters[indexList].count % 2) == 0) {
-                letrasImpares--;
+            lettersCount[indexList].count++;
+            if ((lettersCount[indexList].count % 2) == 0) {
+                notPairsCount--;
             } else {
-                letrasImpares++;
+                notPairsCount++;
             }
         } else {
-            totalLetters.push({ letter: element, count: 1 });
-            letrasImpares++;
+            lettersCount.push({ letter: element, count: 1 });
+            notPairsCount++;
         }
     }
     // Verificacion si no es palindromo
-    const esParTotalLetras = letters.length;
-    let indiceDerecho = 0;
-    let indiceIzquierdo = 0;
-    let indiceCambia = 0;
-    let letraUno = '';
-    let letraDos = '';
-    let comparacion1 = []
-    let reverseWordUltimo = []
-    if (((esParTotalLetras % 2) != 0 && letrasImpares == 1) ||
-        ((esParTotalLetras % 2) == 0 && letrasImpares == 0)) {
+    const totalLetters = letters.length;
+    let rightIndex = 0;
+    let leftIndex = 0;
+    let changeIndex = 0;
+    let letterA = '';
+    let letterB = '';
+    let textMod = []
+    let rvWordMod = []
+    if (((totalLetters % 2) != 0 && notPairsCount == 1) ||
+        ((totalLetters % 2) == 0 && notPairsCount == 0)) {
         for (let index = 0; index < letters.length; index++) {
-            indiceDerecho = index;
-            indiceIzquierdo = (letters.length - 1) - index;
-            letraUno = letters[indiceDerecho];
-            letraDos = letters[indiceIzquierdo];
-            if (letraUno != letraDos) {
+            rightIndex = index;
+            leftIndex = (letters.length - 1) - index;
+            letterA = letters[rightIndex];
+            letterB = letters[leftIndex];
+            if (letterA != letterB) {
                 // Si las letras son diferentes vamos a obtener
-                indiceCambia = letters.indexOf(letraDos);
-                if (indiceCambia == indiceIzquierdo) {
-                    indiceCambia = reverseWord.indexOf(letraUno)
-                    return [indiceCambia, indiceIzquierdo]
+                changeIndex = letters.indexOf(letterB);
+                if (changeIndex == leftIndex) {
+                    changeIndex = reverseWord.indexOf(letterA)
+                    return [changeIndex, leftIndex]
                 } else {
-                    comparacion1 = word.split('')
-                    comparacion1[indiceDerecho] = letters[indiceCambia]
-                    comparacion1[indiceCambia] = letters[indiceDerecho]
-                    reverseWordUltimo = [...comparacion1].reverse();
+                    textMod = word.split('')
+                    textMod[rightIndex] = letters[changeIndex]
+                    textMod[changeIndex] = letters[rightIndex]
+                    rvWordMod = [...textMod].reverse();
                     break;
                 }
             }
         }
 
-        for (let index = 0; index < reverseWordUltimo.length; index++) {
-            if (comparacion1[index] != reverseWordUltimo[index]) {
+        for (let index = 0; index < rvWordMod.length; index++) {
+            if (textMod[index] != rvWordMod[index]) {
                 return null
             }
         }
-        return [indiceDerecho, indiceCambia];
+        return [rightIndex, changeIndex];
     }
     return null;
 }
+
+
+getIndexsForPalindrome('anna') // []
+getIndexsForPalindrome('abab') // [0, 1]
+getIndexsForPalindrome('abac') // null
+getIndexsForPalindrome('aaaaaaaa') // []
+getIndexsForPalindrome('aaababa') // [1, 3]
+getIndexsForPalindrome('caababa') // null
